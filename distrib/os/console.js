23eebd9fd9ca14f459f36029df0bca1c103033d7
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-09-12 17:26:49 +0000

    Backspace working but not deleting all of the letter

--- distrib/os/console.js
+++ distrib/os/console.js
@@ -61,7 +61,7 @@ var TSOS;
             // console.log('TESTING LINE FOR BACKSPACE');
             var offset = _DrawingContext.measureText(this.currentFont, this.currentFontSize, text);
             this.currentXPosition = this.currentXPosition - offset;
-            _DrawingContext.clearRect(this.currentXPosition, this.currentYPosition, offset, this.currentFontSize);
+            _DrawingContext.clearRect(this.currentXPosition, this.currentFontSize, offset, this.currentFontSize);
         }
         putText(text) {
             /*  My first inclination here was to write two functions: putChar() and putString().
