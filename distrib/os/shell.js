d04099000bb2496cab2b54fe7a72f562a981f9ff
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-09-15 23:17:48 +0000

    Trying fix

--- distrib/os/shell.js
+++ distrib/os/shell.js
@@ -105,7 +105,7 @@ var TSOS;
             }
             if (found) {
                 this.execute(fn, args); // Note that args is always supplied, though it might be empty.
-                this.commandsUsed.push(cmd);
+                // this.commandsUsed.push(cmd);
             }
             else {
                 // It's not found, so check for curses and apologies before declaring the command invalid.
