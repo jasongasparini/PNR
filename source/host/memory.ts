59353ee3026151d53b1ecc8ccec4911c7fd89766
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-09-29 21:02:49 +0000

    Testing memory view

--- source/host/memory.ts
+++ source/host/memory.ts
@@ -42,13 +42,16 @@
             return hexValue & 0xFF; // Ensure that the value is within the range of 0x00 to 0xFF
         }
 
-        public updateMemoryTable() {
+        public updateMemoryTable(): void {
             const tableBody = document.querySelector('#memoryTable tbody');
             tableBody.innerHTML = '';
         
             for (let address = 0; address < 256; address++) {
-              const hexAddress = address.toString(16).toUpperCase().padStart(2, '0');
-              const hexValue = this.readByte(address);
+            //   const hexAddress = address.toString(16).toUpperCase().padStart(2, '0');
+            //   const hexValue = this.readByte(address);
+
+            const hexAddress = address;
+            const hexValue = 1
               
               const row = `<tr>
                              <td>0x${hexAddress}</td>
