32f0cf1f478e954e29ffa95c49d99c88a99d6d49
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-09-29 21:06:57 +0000

    Testing memory view

--- source/host/cpu.ts
+++ source/host/cpu.ts
@@ -41,7 +41,7 @@ module TSOS {
             // TODO: Accumulate CPU usage and profiling statistics here.
             // Do the real work here. Be sure to set this.isExecuting appropriately.
             
-            _Memory.updateMemoryTable();
+            
 
         }
 
