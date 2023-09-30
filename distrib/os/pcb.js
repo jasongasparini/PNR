8c9754857b884ea020df22b3e866d79614c5ccde
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-09-30 15:51:21 +0000

    created PCB constructor and moved taskbar

--- distrib/os/pcb.js
+++ distrib/os/pcb.js
@@ -6,7 +6,27 @@
 var TSOS;
 (function (TSOS) {
     class ProcessControlBlock {
-        constructor() {
+        PID = 0;
+        PC = 0;
+        IR = 0x00;
+        ACC = 0x00;
+        X = 0;
+        Y = 0;
+        Z = 0;
+        priority = 0;
+        state = "";
+        location = 0;
+        constructor(pid, pc, ir, acc, xReg, yReg, zFlag, prior, sta, loca) {
+            this.PID = pid;
+            this.PC = pc;
+            this.IR = ir;
+            this.ACC = acc;
+            this.X = xReg;
+            this.Y = yReg;
+            this.Z = zFlag;
+            this.priority = prior;
+            this.state = sta;
+            this.location = loca;
         }
     }
     TSOS.ProcessControlBlock = ProcessControlBlock;
--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -6,8 +6,31 @@
 
 module TSOS {
     export class ProcessControlBlock {
+        public PID: number = 0;
+        public PC: number = 0;
+        public IR: number = 0x00;
+        public ACC: number = 0x00;
+        public X: number = 0;
+        public Y: number = 0;
+        public Z: number = 0;
+        public priority: number = 0;
+        public state: string = "";
+        public location: number = 0;
 
-        constructor() {
+        constructor(pid: number, pc: number, ir: number, acc: number,
+                    xReg: number, yReg: number, zFlag: number,
+                    prior: number, sta: string, loca: number) {
+
+            this.PID = pid;
+            this.PC = pc;
+            this.IR = ir;
+            this.ACC = acc;
+            this.X = xReg;
+            this.Y = yReg;
+            this.Z = zFlag;
+            this.priority = prior;
+            this.state = sta;
+            this.location = loca;
 
         }
     }
