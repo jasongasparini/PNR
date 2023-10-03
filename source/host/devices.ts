d569d25e53d21aacc9c089f8037133e57d015d3a
jasongasparini <123187923+jasongasparini@users.noreply.github.com>
2023-10-03 04:44:44 +0000

    adding gui update to clockpulse

--- source/host/devices.ts
+++ source/host/devices.ts
@@ -31,6 +31,9 @@ module TSOS {
             _OSclock++;
             // Call the kernel clock pulse event handler.
             _Kernel.krnOnCPUClockPulse();
+
+            _CPU.updateTable();
+            _Memory.updateMemoryTable(); 
         }
 
         //
